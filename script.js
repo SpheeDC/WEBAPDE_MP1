9f1c54317d6df0f9fbca10819276ccc3705f858b
paoloandreiseril <paolo_andrei02@yahoo.com>
2017-06-13 00:35:48 +0800

    Updated CSS, HTML and JS files

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
 /**
  * Created by paoloandreiseril on 6/10/17.
  */
+
+var root = 'https://jsonplaceholder.typicode.com';
+var users = [];
+
+function loadData() {
+    for (var i = 1; i <= 10; i++) {
+        getUser(i);
+        console.log(users[i - 1]);
+        users[i - 1].posts = [];
+        for (var j = 1; j <= 100; j++) {
+
+            var post = getPost(j);
+            if (post.userId === users[i - 1].id) {
+                users[i].posts.push(post);
+            }
+        }
+    }
+}
+function getUser(id) {
+    $.ajax({
+        url: root + '/users/' + id,
+        method: 'GET'
+    }).then(function(data) {
+        users.push(data);
+    });
+}
+
+function getPost(id) {
+    $.ajax({
+        url: root + '/posts/' + id,
+        method: 'GET'
+    }).then(function (data) {
+        /** @namespace data.userId */
+        /** @namespace data.title */
+        /** @namespace data.body */
+        return {
+            "userID": data.userId,
+            "title": data.title,
+            "post": data.body
+        }
+    });
+}
+
+
+
