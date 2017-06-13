92074734c47c61a51913ecd1b6732e5390d89c35
paoloandreiseril <paolo_andrei02@yahoo.com>
2017-06-13 17:07:48 +0800

    Updated webpage.

--- script.js
+++ script.js
@@ -9,11 +9,12 @@ function loadData() {
     for (var i = 1; i <= 10; i++) {
         getUser(i);
         console.log(users[i - 1]);
-        users[i - 1].posts = [];
+        //var posts = [];
+        //users[i - 1].posts = posts;
         for (var j = 1; j <= 100; j++) {
 
             var post = getPost(j);
-            if (post.userId === users[i - 1].id) {
+            if (post.userId === users[i - 1].val('id')) {
                 users[i].posts.push(post);
             }
         }
@@ -42,6 +43,38 @@ function getPost(id) {
             "post": data.body
         }
     });
+}
+
+function viewMorePosts() {
+    //var postsSection = document.getElementById('posts');
+    for (var i = 1; i <= 10; i++) {
+        /*
+         <br>
+         <article>
+         <br>
+         <div class="profilePic">
+
+
+         </div>
+
+         </article>
+         <br>
+         */
+    }
+}
+
+function hideBackground() {
+    document.getElementById('grayHideBackground').style.display = "block";
+    document.getElementById('listBtn').style.color = "white";
+}
+
+/*function showBackground() {
+    document.getElementById('grayHideBackground').style.display = "none";
+    document.getElementById('listBtn').style.color = "black";
+}*/
+
+function goToProfile() {
+    console.log("I'm clicked.");
 }
 
 
