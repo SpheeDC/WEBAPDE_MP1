fe0ac2e6926eb2dc23e7dfa427d34eb2c178c71f
paoloandreiseril <paolo_andrei02@yahoo.com>
2017-06-19 00:33:03 +0800

    :)

--- Scripts/photos.js
+++ Scripts/photos.js
@@ -1,4 +1,5 @@
 var photos = [];
+var counter = 0;
 
 function loadPhotos() {
     // get all photos first from the fake server and store it in a local JSON array
@@ -10,18 +11,42 @@ function loadPhotos() {
         photos = data;
     });
 
-    var photoc = $("#photocontainer");
-    // place in the first 9 photos in each specified div .class
-    for (var i = 1; i <= 9; i++) {
-        var img = $(document).createElement('img');
-        img.src = photos[i - 1].thumbnailUrl;
-
-        photoc.appendChild(img);
+    var photoc = document.getElementById('photocontainer');
+    // place in the first 10 photos in each specified div .class
+    for (var i = 1; i <= 10; i++) {
+        var newdiv = document.createElement('div');
+        newdiv.setAttribute('class', 'pic');
+        var img = document.createElement('img');
+        img.src = photos[i - 1]['thumbnailUrl'];
+        newdiv.appendChild(img);
+        photoc.appendChild(newdiv);
     }
+
+    counter += 10;
 
 }
 function viewMorePhotos() {
     // place in the next 9 photos to view
+    /* <div class="pic">
+            <img src="Images/picture-placeholder.png">
+        </div> 
+    */
+    if (counter === 5000) {
+        alert("No more photos to show.");
+    }
+    else {
+        var photoc = document.getElementById('photocontainer');
+        for (var i = 1; i <= 10; i++) {
+            var newdiv = document.createElement('div');
+            newdiv.setAttribute('class', 'pic');
+            var img = document.createElement('img');
+            img.src = 'Images/picture-placeholder.png';
+            newdiv.appendChild(img);
+            photoc.appendChild(newdiv);
+        }
+
+        counter += 10;
+    }
 }
 
 function getAllPhotosFrom(albumId) {
--- Scripts/script.js
+++ Scripts/script.js
@@ -2,25 +2,24 @@
  * Created by paoloandreiseril on 6/10/17.
  */
 var root = 'https://jsonplaceholder.typicode.com';
-// users JSON array should contain all info from /users JSON objects
 var users = [];
-// contains id of post, user who made the post, and the content
 var posts = [];
-
-function about() {
-    alert('We are the HTML, CSS, Javascript Developers and Swift soon. :)\nWelcome to the Fakes.');
-}
+var counter = 0;
 
 function loadData() {
+    "use strict";
     getAllUsers();
+    getAllPosts();
 
-    for (var i = 1; i <= 9; i++) {
-        document.getElementById('name' + i).innerHTML = users[i - 1].attr("username");
+    // show only the first 10 posts
+    for (var i = 0; i <= 9; i++) {
+        $("#title" + i).text(posts[i]['title']);
+        $("#post" + i).text(posts[i]['body']);
     }
-
-    // set up the posts
+    counter += 10;
 }
 function getAllUsers() {
+    "use strict";
     $.ajax({
         url: root + '/users/',
         method: 'GET'
@@ -29,31 +28,37 @@ function getAllUsers() {
     });
 }
 
+function getAllPosts() {
+    $.ajax({
+        url: root + '/posts/',
+        method: 'GET'
+    }).then(function (data) {
+        posts = data;
+    })
+}
+
+function getUser(userId) {
+    // function body to follow
+}
 // for the profile of each user
 function getPostsOfUser(id) {
     $.ajax({
         url: root + '/users/' + id + '/posts',
         method: 'GET'
     }).then(function (data) {
-
+        // function body to follow
     })
 }
 
 // should make use of document.createElement(), and element.setAttribute()
 function viewMorePosts() {
-
+    if (counter === 100) {
+        alert("No more posts to show.\nSad!");
+    }
+    else {
+        
+    }
 }
-
-function hideBackground() {
-    document.getElementById('grayHideBackground').style.display = "block";
-    document.getElementById('listBtn').style.color = "white";
-}
-
-/*function showBackground() {
-    document.getElementById('grayHideBackground').style.display = "none";
-    document.getElementById('listBtn').style.color = "black";
-}*/
-
 /*function goToProfile(userID) {
     console.log("I'm clicked.");
 }
