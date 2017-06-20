bfd907862b8b3f180e133bff99fd41532410587b
paoloandreiseril <paolo_andrei02@yahoo.com>
2017-06-21 06:20:42 +0800

    :)

---
+++ Scripts/profile.js
@@ -0,0 +1,5 @@
+function loadUserProfile(User) {
+    "use strict";
+    
+    // load the User object into each of the divs / containers
+}
--- Scripts/script.js
+++ Scripts/script.js
@@ -5,17 +5,69 @@ var root = 'https://jsonplaceholder.typicode.com';
 var users = [];
 var posts = [];
 var counter = 0;
-
 function loadData() {
-    "use strict";
     getAllUsers();
     getAllPosts();
 
+    var postSection = document.getElementById('postSection');
+
+    /*
+     <div class="leftColumn">
+     <br>
+     <div class="profilePic">
+     </div>
+     <div class="content">
+     <p class="username">
+     username
+     </p>
+     <p class="title">
+     title
+     </p>
+     <p class="actualPost">
+     akjuhyb ijuygtv uhy6tfr uhygb aasdnhb
+     </p>
+     </div>
+     </div>
+     */
     // show only the first 10 posts
     for (var i = 0; i <= 9; i++) {
-        $("#title" + i).text(posts[i]['title']);
-        var newuser = getUser(posts[i]['userId']);
-        $("#post" + i).text(posts[i]['body']);
+
+        // get all the needed content
+
+        console.log(i);
+        console.log(posts[i]);
+        var mainContent = posts[i]['body'];
+        var userIdP = getUser(posts[i]['userId']);
+        var nameOfUser = document.createTextNode(userIdP);
+        var realTitle = document.createTextNode(String(posts[i]["title"]));
+        var contentReal = document.createTextNode(mainContent);
+
+        var newdiv = document.createElement('div');
+        newdiv.setAttribute('class', 'leftColumn');
+        newdiv.appendChild(document.createElement('br'));
+        var profileImageDiv = document.createElement('div');
+        profileImageDiv.setAttribute('class', 'profilePic');
+
+        var contentDiv = document.createElement('div');
+        contentDiv.setAttribute('class', 'content');
+
+        var usernamep = document.createElement('p');
+        usernamep.setAttribute('class', 'username');
+        usernamep.appendChild(nameOfUser);
+
+        var titlep = document.createElement('p');
+        titlep.setAttribute('class', 'title');
+        titlep.appendChild(realTitle);
+
+        var actualPostp = document.createElement('p');
+        actualPostp.setAttribute('class', 'actualPost');
+        actualPostp.appendChild(contentReal);
+
+        contentDiv.appendChild(usernamep);
+        contentDiv.appendChild(titlep);
+        contentDiv.appendChild(actualPostp);
+
+        postSection.appendChild(contentDiv);
     }
     counter += 10;
     console.log(counter);
@@ -40,10 +92,9 @@ function getAllPosts() {
 }
 
 function getUser(userId) {
-    // function body to follow
     for (var i = 0; i < users.length; i++) {
         if (users[i]['id'] === userId) {
-            return users[i];
+            return users[i]['username'];
         }
     }
     return null;
